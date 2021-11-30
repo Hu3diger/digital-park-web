cee6ba102697f9efa1f3e08f494a8691134038cd
Hu3diger <martin@ibtech.inf.br>
2021-11-30 13:45:11 -0300

    Alterado para autenticação via angular

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,28 +1,68 @@
-import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { Auth } from 'src/app/model/auth/Auth';
 import { HttpClient } from '@angular/common/http';
 import { BaseService } from './base.service';
-import { ResponseModel } from '../model/ResponseModel';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ParkUser } from '../model/ParkUser';
 
 @Injectable()
 export class AuthService extends BaseService {
 
-	restUrl: string;
+	isAuthenticated = false;
 
 	constructor(
-		readonly http: HttpClient
+		readonly http: HttpClient,
+		readonly storage: AngularFirestore
 	){
 		super();
-		this.restUrl = this.baseUrl + '/Users'
 	}
 
-	autheticate(user: Auth): Promise<ResponseModel<Auth>>  {
-		return this.http.post<ResponseModel<Auth>>(this.restUrl + '/auth', user).toPromise();
+	setFields(object: any): ParkUser {
+		if (object == null || object == undefined){
+			return null;
+		}
+
+		var user = new ParkUser();
+		user.email = object.email;
+		user.password = object.password;
+		user.fullName = object.fullName;
+		user.username = object.username;
+		user.active = true;
+
+		user.roles = new Array<any>();
+		if (object.roles !== undefined){
+			object.roles.forEach(async (el) => {
+				let data = {
+					id: el.id,
+					path: el.path
+				}
+				user.roles.push(data);
+			});
+		}
+
+		return user;
 	}
 
-	save(user: Auth): Promise<ResponseModel<Auth>> {
-		return this.http.post<ResponseModel<Auth>>(this.restUrl + '/register', user).toPromise()
+	async autheticate(user: Auth): Promise<ParkUser>  {
+		debugger
+		var userRef = await this.storage.collection('users').doc(user.username).get().toPromise();
+		var parkUser = this.setFields(userRef.data());
+
+		if (parkUser != null){
+			if (parkUser.password != user.password) {
+				throw 'Senha incorreta!';
+			} else {
+				this.isAuthenticated = true;
+				return parkUser;
+			}
+		} else {
+			throw 'Usuário inválido!';
+		}
+
+		
+	}
+
+	save(user: Auth): void {
 	}
 
 }
